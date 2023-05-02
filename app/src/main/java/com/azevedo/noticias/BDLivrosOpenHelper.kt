fc0b8e4943f4c30d335abcbe5556f99406bea005
arman <armandojpazevedo@gmail.com>
2023-05-02 10:40:10 +0100

    Defenir o nome e a versão da base de dados

--- app/src/main/java/com/azevedo/noticias/BDLivrosOpenHelper.kt
+++ app/src/main/java/com/azevedo/noticias/BDLivrosOpenHelper.kt
@@ -4,17 +4,17 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+private const val NOME_BASE_DADOS = "Noticias.db"
+private const val VERSAO_BADE_DADOS = 1
+
 class BDLivrosOpenHelper(
     context: Context?,
-    name: String?,
-    factory: SQLiteDatabase.CursorFactory?,
-    version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
+) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BADE_DADOS) {
     override fun onCreate(p0: SQLiteDatabase?) {
         TODO("Not yet implemented")
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    override fun onUpgrade(p0: SQLiteDatabase?, oldVersion : Int, newVersion: Int) {
         TODO("Not yet implemented")
     }
 }
