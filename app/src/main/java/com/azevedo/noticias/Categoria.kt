a84eca34230a09005cdf6cc6114605ba50f71741
arman <armandojpazevedo@gmail.com>
2023-05-12 09:06:23 +0100

    Criar a classe que toContentVaules para a Tabela_Categorias

--- app/src/main/java/com/azevedo/noticias/Categoria.kt
+++ app/src/main/java/com/azevedo/noticias/Categoria.kt
@@ -1,10 +1,16 @@
 package com.azevedo.noticias
 
-data class Categoria(
-    var nome: String,
-    var descricao: String,
-    var id: Long = -1
-    ){
+import android.content.ContentValues
+
+data class Categoria(var nome: String, var descricao: String, var id: Long = -1) {
+
+    fun toContentVaules(): ContentValues {
+        val valores = ContentValues()
+
+        valores.put("nome", nome)
+        valores.put("descricao", descricao)
+
+        return valores
+    }
 
 }
-
