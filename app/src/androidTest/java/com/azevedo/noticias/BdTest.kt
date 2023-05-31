988ac10fdc631972d456ae2a6bdffb11d1063741
arman <armandojpazevedo@gmail.com>
2023-05-31 20:24:34 +0100

    Modança de icon da aplicação

--- app/src/androidTest/java/com/azevedo/noticias/BdTest.kt
+++ app/src/androidTest/java/com/azevedo/noticias/BdTest.kt
@@ -13,11 +13,7 @@ import org.junit.Assert.*
 import org.junit.Before
 import java.util.Calendar
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 @RunWith(AndroidJUnit4::class)
 class BdTest {
 
