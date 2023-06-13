9f4539ddffc5f0677bb23be85bf9a50f6c94e186
arman <armandojpazevedo@gmail.com>
2023-06-13 09:42:41 +0100

    Renomear

--- app/src/main/java/com/azevedo/noticias/EliminarNoticia_Fragmento.kt
+++ app/src/main/java/com/azevedo/noticias/EliminarNoticia_Fragmento.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [EleminarNoticia_Fragmento.newInstance] factory method to
+ * Use the [EliminarNoticia_Fragmento.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EleminarNoticia_Fragmento : Fragment() {
+class EliminarNoticia_Fragmento : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class EleminarNoticia_Fragmento : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            EleminarNoticia_Fragmento().apply {
+            EliminarNoticia_Fragmento().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
