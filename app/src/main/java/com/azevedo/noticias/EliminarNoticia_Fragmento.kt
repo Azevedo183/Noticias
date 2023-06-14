209798d2bed5e1bae3ec7f6d33ea7f6442420d76
arman <armandojpazevedo@gmail.com>
2023-06-14 13:16:03 +0100

    Eliminar Noticia

--- app/src/main/java/com/azevedo/noticias/EliminarNoticia_Fragmento.kt
+++ app/src/main/java/com/azevedo/noticias/EliminarNoticia_Fragmento.kt
@@ -1,5 +1,6 @@
 package com.azevedo.noticias
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,8 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.azevedo.noticias.databinding.FragmentEleminarNoticiaFragmentoBinding
 import android.text.format.DateFormat
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class EliminarNoticia_Fragmento : Fragment() {
     private lateinit var noticias: Noticias
@@ -53,17 +56,27 @@ class EliminarNoticia_Fragmento : Fragment() {
                 true
             }
             R.id.action_cancelar -> {
-                voltaListaLivros()
+                voltaListaNoticias()
                 true
             }
             else -> false
         }
     }
 
-    private fun voltaListaLivros() {
+    private fun voltaListaNoticias() {
         findNavController().navigate(R.id.action_eleminarNoticia_Fragmento_to_ListaNoticias_Fragment)
     }
 
     private fun eliminar() {
+        val enderecoNoticia = Uri.withAppendedPath(NoticiasContentProvider.ENDERECO_NOTICIAS, noticias.id.toString())
+        val numNoticiaSelecionadas = requireActivity().contentResolver.delete(enderecoNoticia,null, null)
+
+        if (numNoticiaSelecionadas == 1){
+            Toast.makeText(requireContext(), getString(R.string.noticia_eliminada_com_sucesso), Toast.LENGTH_LONG).show()
+            voltaListaNoticias()
+        } else{
+            Snackbar.make(binding.textViewTituloEliminar, getString(R.string.erro_eliminar_noticia), Snackbar.LENGTH_INDEFINITE)
+
+        }
     }
 }
