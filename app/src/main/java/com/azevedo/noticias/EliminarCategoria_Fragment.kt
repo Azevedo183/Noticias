9b646805783d4567ee8904845401feffa193d0b2
arman <armandojpazevedo@gmail.com>
2023-06-18 14:05:35 +0100

    Implementação do eliminar categoria(com erro)

--- app/src/main/java/com/azevedo/noticias/EliminarCategoria_Fragment.kt
+++ app/src/main/java/com/azevedo/noticias/EliminarCategoria_Fragment.kt
@@ -1,59 +1,80 @@
 package com.azevedo.noticias
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.azevedo.noticias.databinding.FragmentEliminarCategoriaBinding
+import com.google.android.material.snackbar.Snackbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EliminarCategoria_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EliminarCategoria_Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var categoria: Categoria
+    private var _binding: FragmentEliminarCategoriaBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eliminar_categoria_, container, false)
+
+        _binding = FragmentEliminarCategoriaBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EliminarCategoria_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EliminarCategoria_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_eleminar
+
+        categoria = EliminarCategoria_FragmentArgs.fromBundle(requireArguments()).categorias
+
+        binding.textViewCategoriaCEliminar.text = categoria.nome
+        binding.textViewDescricaoCEliminar.text = categoria.descricao
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_eliminar -> {
+                eliminar()
+                true
             }
+            R.id.action_cancelar -> {
+                voltaListaCategorias()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun voltaListaCategorias() {
+        findNavController().navigate(R.id.action_eliminarCategoria_Fragment_to_listaCategorias_Fragment)
+    }
+
+    private fun eliminar() {
+        val enderecoCategorias = Uri.withAppendedPath(NoticiasContentProvider.ENDERECO_CATEGORIA, categoria.id.toString())
+        val numCategoriaSelecionadas = requireActivity().contentResolver.delete(enderecoCategorias,null, null)
+
+        if (numCategoriaSelecionadas == 1){
+            Toast.makeText(requireContext(), getString(R.string.categoria_eliminada_com_sucesso), Toast.LENGTH_LONG).show()
+            voltaListaCategorias()
+        } else{
+            Snackbar.make(binding.textViewCategoriaCEliminar, getString(R.string.erro_ao_eliminar_categoria), Snackbar.LENGTH_INDEFINITE)
+
+        }
     }
 }
